eb1a45c61397f63e57f1465f21b0494813636ebf
Nnvedward <nnvedward@gmail.com>
2022-01-17 14:53:39 +0100

    Styling the indecision application components

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,14 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleDeleteSelectedOption}
         contentLabel="Selected Option"
+        appElement={app}
+        closeTimeoutMS={200}
+        className='modal'
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
         <button
+            className="button"
             onClick={props.handleDeleteSelectedOption}
         >
             Ok
